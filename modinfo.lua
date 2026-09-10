01a148294e6cb7d657fe0d3cb9c4d7927fc5a4fb
SuperSayianZilla <48083027+SuperSayianZilla@users.noreply.github.com>
2026-09-10 12:52:48 -0400

    Add files via upload

---
+++ modinfo.lua
@@ -0,0 +1,12 @@
+return {
+	name = 'Supreme-K',
+	description = 'New Facs',
+	shortname = 'SK',
+	version = '5.89',
+	game = 'Supreme-K',
+	shortGame = 'Supreme-K',
+	modtype = 1,
+	depend = {
+		[[rapid://zk:stable]]
+	},
+}
